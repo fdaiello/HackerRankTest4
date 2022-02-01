89897467117f554a71623b2c9c882c847a6d7f5f
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-02-01 10:38:59 -0300

    Beadth First Search: Shortest Reach - with bugs

--- HackerRankTest4/Program.cs
+++ HackerRankTest4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HackerRankTest4
 {
@@ -6,7 +7,24 @@ namespace HackerRankTest4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestBFS();
+        }
+
+        static void TestBFS()
+        {
+            int n = 5;
+            int m = 4;
+            int s = 1;
+
+            List<List<int>> edges = new()
+            {
+                new() { 1, 2 },
+                new() { 2, 3 },
+                new() { 2, 4 },
+                new() { 1, 3 }
+            };
+
+            Console.WriteLine(String.Join(",", Result.bfs(n, m, edges, s)));
         }
     }
 }
--- HackerRankTest4/Result.cs
+++ HackerRankTest4/Result.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace HackerRankTest4
 {
+    class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+            Edges = new List<Node>();
+        }
+        public int Value { get; set; }
+        public List<Node> Edges { get; set; }
+    }
     class Result
     {
        /*
@@ -23,11 +33,110 @@ namespace HackerRankTest4
         *  List with the distance of all nodes to starting node - not considering starting node
         *  
         *  
+        *  
+        *  
+        *  BUGS: 
+        *        1) dept counter
+        *        2) Not checking all nodes ( its checking only nodes in map, that is, in graph )
+        *  
         */
 
         public static List<int> bfs(int n, int m, List<List<int>> edges, int s)
         {
 
+            // Return list with depths
+            List<int> rList = new List<int>();
+
+            // Map for nodes
+            Dictionary<int, Node> map = new Dictionary<int, Node>();
+
+            // Build graph
+            for ( int i =0; i < edges.Count; i++)
+            {
+                int n1Value = edges[i][0];
+                int n2Value = edges[i][1];
+
+                // Are nodes already in graph?
+                Node node1;
+                if (map.ContainsKey(n1Value))
+                {
+                    node1 = map[n1Value];
+                }
+                else
+                {
+                    node1 = new Node(n1Value);
+                    map.Add(n1Value, node1);
+                }
+                Node node2;
+                if (map.ContainsKey(n2Value))
+                {
+                    node2 = map[n2Value];
+                }
+                else
+                {
+                    node2 = new Node(n2Value);
+                    map.Add(n2Value, node2);
+                }
+
+                // Link nodes - create edge
+                if ( ! node1.Edges.Contains(node2))
+                    node1.Edges.Add(node2);
+                if ( ! node2.Edges.Contains(node1))
+                    node2.Edges.Add(node1);
+            }
+
+            // Traverse all nodes
+            foreach ( KeyValuePair<int, Node> kv in map)
+            {
+                // - except start node
+                if ( kv.Key != s)
+                {
+                    // start node
+                    Node pn = map[s];
+
+                    // Get depth
+                    int depth = 0;
+                    List<int> visited = new List<int>();
+
+                    if ( GetDepth(pn, kv.Key, ref depth, visited))
+                    {
+                        rList.Add(depth);
+                    }
+                    else
+                    {
+                        rList.Add(-1);
+                    }
+
+                }
+            }
+
+
+            // Return list with dephts
+            return rList;
+        }
+        static bool GetDepth(Node node, int value, ref int depth, List<int> visited)
+        {
+            depth++;
+
+            if ( node.Value == value)
+            {
+                return true;
+            }
+            else
+            {
+                foreach ( Node chilld in node.Edges)
+                {
+                    if ( !visited.Contains(chilld.Value))
+                    {
+                        visited.Add(chilld.Value);
+                        if (GetDepth(chilld, value, ref depth, visited))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
         }
     }
 }
