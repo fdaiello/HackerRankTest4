4b3a97e5d31a638287aa7d0305292a43bfca804b
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-02-01 09:12:30 -0300

    Add project files.

---
+++ HackerRankTest4/Result.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTest4
+{
+    class Result
+    {
+       /*
+        *  Beadth First Search: Shortest Reach:
+        *  
+        *  INPUT
+        *  n - number of nodes
+        *  m - number of vertices
+        *  edges - list of edges ( pairs of nodes that are conected )
+        *  s - starting node
+        *  
+        *  Obs: Consider each edge weight 6 units
+        *  
+        *  OUTPUT
+        *  List with the distance of all nodes to starting node - not considering starting node
+        *  
+        *  
+        */
+
+        public static List<int> bfs(int n, int m, List<List<int>> edges, int s)
+        {
+
+        }
+    }
+}
